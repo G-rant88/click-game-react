539605f4e37a0f5282a8c6c9a12efaf8d5bb203c
Ben Grant <Benjah88@gmail.com>
2018-01-05 12:44:33 -0800

    okok

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -27,68 +27,109 @@ class App extends Component {
 </div>
 </div>
 <div className="row">
-<div className="col s3 hoverable">
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/akbar.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/boba.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/c3.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/chewy.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/emperor.jpeg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/ewok.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/greedo.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/han.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/jabba.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="images/lando.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/leia.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/luke.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/obiwan.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/r2.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/sand.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/slaveleia.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/storm.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/tauntaun.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/vader.jpg"/></a>
 </div>
-<div className="col s3 hoverable">
+</div>
+<div className="col s3">
+<div className="sizing hoverable">
 <a href=""><img className="image" src="/images/yoda.jpg"/></a>
 </div>
 </div>
 </div>
+</div>
+
 <footer class="page-footer">
   <div class="footer-copyright">
             <div class="container">
