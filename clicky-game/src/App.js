f9a9c5a4e84d2dbd6675234247ce43cbb5c41e41
Ben Grant <Benjah88@gmail.com>
2018-01-05 20:02:20 -0800

    done:

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -6,7 +6,10 @@ import ItemCard from "./components/itemCard.js";
 class App extends Component {
 
 state = {
-    items
+    items,
+    Score: 0,
+    TopScore: 0,
+    correct: ""
   };
 
     shuffleItems = () => {
@@ -17,6 +20,42 @@ state = {
   this.setState({ items });
   };
 
+
+  addScore = (val) => {
+    
+  const value = parseInt(val);
+
+    for (let i = 0; i < items.length; i++) {
+
+  if (items[value].clicked === true){
+
+     this.state.correct = "Wrong Choice!";
+
+  if(this.state.Score > this.state.TopScore){
+     this.state.TopScore = this.state.Score;
+   }
+    this.setState({ Score: 0 });
+
+for (let i = 0; i < items.length; i++) {
+
+  items[i].clicked = false;
+
+  }
+  return false;
+}
+
+    }
+  
+
+   this.state.Score = this.state.Score + 1;
+   this.state.correct = "Correct Choice!";
+
+   items[value].clicked = true;
+
+
+  };
+
+
   render() {
     return (
       <div>
@@ -24,11 +63,11 @@ state = {
        <nav>
     <div className="nav-wrapper">
       <a href="" className="brand-logo logo">Star Wars Game</a>
-      <a className="brand-logo center">Guess Here</a>
+      <a className="brand-logo center">{this.state.correct}</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li>Score: 0</li>
+        <li>Your Score: {this.state.Score}</li>
         <li>|</li>
-        <li>Top Score: 0</li>
+        <li>Top Score: {this.state.TopScore}</li>
       </ul>
     </div>
   </nav>
@@ -36,7 +75,7 @@ state = {
   <div className="container">
 <div className="row">
 <div className="col s12 top z-depth-2">
-<h2 className="center">Star Wars - The Good Ones - Memory Click Game</h2>
+<h2 className="center">The Good Star Wars Movies - Memory Click Game</h2>
 <h4 className="center">Click on an image to earn points, but don't click the same image more than once!</h4>
 </div>
 </div>
@@ -47,7 +86,10 @@ state = {
   alty={item.name} 
   keyy={item.id} 
   srcy={item.image}
+  clicky={item.clicked}
   shuffle={this.shuffleItems}
+  add={this.addScore}
+
 />
 ))}
 
--- clicky-game/src/components/itemCard.js
+++ clicky-game/src/components/itemCard.js
@@ -3,8 +3,9 @@ import '../App.css';
 
 const ItemCard = props => (
   <div className="col s3">
-    <div className="sizing hoverable" onClick={ () => props.shuffle()}>
-      <img className="image" alt={props.alty} src={props.srcy} value={props.keyy} />
+    <div className="sizing hoverable" onClick={() => props.shuffle()}>
+      <img onClick={() =>props.add(props.keyy)} className="image" alt={props.alty} 
+      src={props.srcy} value={props.keyy} />
   </div>
   </div>
 );
